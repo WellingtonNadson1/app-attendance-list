df497b58dbc73c013e40a2977b0a29ef43562c3c
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-07-27 00:13:10 -0300

    Adjust in the components and consuming of api with fetch

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,16 +1,19 @@
 import './styles.css'
 
-interface data {
+export type dataCard = {
     name: string;
-    date: string;
+    time: string;
 }
 
-export function Card(props: data) {
+export function Card({name, time}: dataCard) {
+    // Posso desestruturar o props que seria colocado
+    // no argumento da função colocando {}, desta forma 
+    // já pego as propriedades de forma direta.
 
     return (
         <div className='card'>
-            <strong>{props.name}</strong>
-            <small>{props.date}</small>
+            <strong>{name}</strong>
+            <small>{time}</small>
         </div>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,43 +1,66 @@
-import { useState } from 'react'
 import './styles.css'
-import { Card } from '../../components/card'
+import { useEffect, useState } from 'react'
+import { Card, dataCard } from '../../components/card'
 
 export function Home() {
 
    const [participanteName, setParticipanteName] = useState('');
-   const [participantes, setParticipantes] = useState([] as any);
+   const [participantes, setParticipantes] = useState<dataCard[]>([]);
+   const [user, setUser] = useState({name: '', avatar_url: ''})
 
 
    function handleAddParticipante() {
     const newParticipante = {
         name: participanteName,
+        time: new Date().toLocaleTimeString("pt-br", {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+        })
     }
-    setParticipantes([newParticipante])
+    setParticipantes(prevState => [...prevState, newParticipante])
    }
-
+//    useEffect é exwcutado apenas uma única vez quando 
+//    a página for carregada se o array de dependências estiver vazio.
+ useEffect(() =>{
+    fetch('https://api.github.com/users/WellingtonNadson1')
+    .then(response => response.json())
+    .then(result => setUser(result))
+   }, [])
+console.log(user)
     return (
-        <div className='container'>
-            <h1>Lista de Presença</h1>
-            <input 
-                type="text" 
-                name="" 
-                id="" 
-                placeholder="Digite seu nome..."
-                onChange={e => setParticipanteName(e.target.value)}
-            />
+        <>
+            <div className='container'>
+                <header>
+                    <h1>Lista de Presença</h1>
+                    <div>
+                        <strong>{user.name}</strong>
+                        <img src={user.avatar_url} alt="Foto do Perfil" />
+                    </div>
+                </header>
+                <input 
+                    type="text" 
+                    name="" 
+                    id="" 
+                    placeholder="Digite seu nome..."
+                    onChange={e => setParticipanteName(e.target.value)}
+                />
 
-            <button 
-                onClick={handleAddParticipante} 
-                type="submit">
-                    Adicionar
-            </button>
-        {
-            participantes.map((p: { name: string; }) => {
-                <Card name={p.name} date="29/06/2022" />
+                <button 
+                    onClick={handleAddParticipante} 
+                    type="submit">
+                        Adicionar
+                </button>
 
-            })
-        }
+            {
+                participantes.map(participante => 
 
-        </div>
+                    <Card name={participante.name} time={participante.time} key={participante.time} />
+
+                )
+            }
+            
+            </div>
+        </>
     )
 }
